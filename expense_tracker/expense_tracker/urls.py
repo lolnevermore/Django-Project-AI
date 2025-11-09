f064c1dc90a02bccaf375c89eb7b9a9b9d23a83a
lolnevermore <79845004+lolnevermore@users.noreply.github.com>
2025-11-09 19:54:46 +0000

    Delete expense_tracker directory

--- expense_tracker/expense_tracker/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('expenses.urls')),  # This includes all expenses URLs
-]
