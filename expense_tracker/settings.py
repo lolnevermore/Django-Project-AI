9be9e77024789abfb8c788a60c084a7aa133db23
lolnevermore <79845004+lolnevermore@users.noreply.github.com>
2025-11-30 03:22:14 +0000

    Update for SQLite

--- expense_tracker/settings.py
+++ expense_tracker/settings.py
@@ -89,10 +89,9 @@ WSGI_APPLICATION = 'expense_tracker.wsgi.application'
 # ----------------------
 # Use PostgreSQL in production
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
+    )
 }
 
 if os.environ.get('DATABASE_URL'):
