b36650b9f2c7d3f3379c835fab464b5507c13def
lolnevermore <79845004+lolnevermore@users.noreply.github.com>
2025-12-01 01:46:02 +0000

    update

--- expenses/urls.py
+++ expenses/urls.py
@@ -27,8 +27,7 @@ urlpatterns = [
     path('budgets/add/', views.budget_create, name='budget_create'),
     path('budgets/<int:pk>/delete/', views.budget_delete, name='budget_delete'),
 
+    # API
     path('api-auth/', include('rest_framework.urls')),
     path('api/expenses/', views.expense_api, name='expense_api'),
-    path('register/', views.register, name='register'),
-    path('activate/<uidb64>/<token>/', views.activate, name='activate'),
 ]
